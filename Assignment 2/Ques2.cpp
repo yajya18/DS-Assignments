7e29a7e65ef123bfea3d93315cf8590742d3ac67
yajya18 <yajyaarora18@gmail.com>
2025-08-21 12:30:54 +0530

    Add files via upload

---
+++ Assignment 2/Ques2.cpp
@@ -0,0 +1,13 @@
+void bubble_sort(int arr[], int size) {
+    bool swapped;
+    for (int i = 0; i < size - 1; ++i) {
+        swapped = false;
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
